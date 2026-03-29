42cf3aeb9c3e975e871ea26fd68233498831c591
BCHoan <183281071+BCHoan@users.noreply.github.com>
2026-03-29 10:01:43 +0700

    danhsachsp

--- routers/web.php
+++ routers/web.php
@@ -6,4 +6,10 @@ $act = $_GET['act'] ?? '/';
 match ($act) {
   // ─── Home mặc định → trang chủ khách hàng ─────────────────────────
   '/' => (new HomeController())->home(),
+
+
+  //customer
+  'home' => (new HomeController())->home(),
+  'books' => (new BookController())->list(),
+  'book-detail' => (new BookController())->detail(),
 };
