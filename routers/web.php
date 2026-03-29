3bd50d63f7935dfa41de6d4d55b42964c481aba8
cuong2103 <dangcuong2103@gmail.com>
2026-03-29 12:05:32 +0700

    fix: lỗi config

--- routers/web.php
+++ routers/web.php
@@ -5,4 +5,17 @@ $act = $_GET['act'] ?? '/';
 match ($act) {
   // ─── Home mặc định → trang chủ khách hàng ─────────────────────────
   '/' => (new HomeController())->home(),
+
+  // ─── Auth ───────────────────────────────────────────────────────
+  'login' => (new AuthController())->formLogin(),
+  'check-login' => (new AuthController())->login(),
+  'register' => (new AuthController())->formRegister(),
+  'check-register' => (new AuthController())->register(),
+  'logout' => (new AuthController())->logout(),
+
+
+  //customer
+  'home' => (new HomeController())->home(),
+  'books' => (new BookController())->list(),
+  'book-detail' => (new BookController())->detail(),
 };
